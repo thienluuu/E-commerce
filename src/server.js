b9f4d9ab5e953a1e98558e7197fb4a189387c603
CrissNguyen <thienm199x@gmail.com>
2023-04-09 11:58:23 +0700

    newa

--- src/server.js
+++ src/server.js
@@ -8,32 +8,32 @@ const viewEngine = require("./config/viewEngine");
 const initWebRoutes = require("./Routes/route");
 
 const app = express();
-// const http = require("http");
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000", // React port
-//   },
-// });
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // React port
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log("user connected");
-//   socket.on("setup", (userData) => {
-//     socket.join(userData.token);
-//     socket.emit("connected");
-//   });
-//   socket.on("join chat", (room) => {
-//     socket.join(room);
-//     console.log("User Joined Room: " + room);
-//   });
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("setup", (userData) => {
+    socket.join(userData.token);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
 
-//   socket.on("private-chat", (message) => {
-//     console.log(message);
-//     socket.to(message.to).emit("private-chat", message);
-//     io.emit("private-chat", message);
-//   });
-// });
+  socket.on("private-chat", (message) => {
+    console.log(message);
+    socket.to(message.to).emit("private-chat", message);
+    io.emit("private-chat", message);
+  });
+});
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
@@ -70,6 +70,9 @@ initWebRoutes(app);
 connectDB();
 const PORT = process.env.PORT || 3030;
 
+server.listen(8080, () => {
+  console.log("Listening on port:" + 8080);
+});
 app.listen(PORT, () => {
   console.log("Server running on:" + PORT);
 });
